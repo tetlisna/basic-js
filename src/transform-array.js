e8c77b514c43045f5fec595ef529b8c768d3e9ca
Tanya Lisna <tplovic@gmail.com>
2023-04-16 20:39:03 +0300

    whatseason ...

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,21 +3,46 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+	const trArr = [...arr];
+	if (trArr instanceof Array === false ) {
+		throw new Error(`'arr' parameter must be an instance of the Array!`);
+	}
+	for (let i = 0; i < trArr.length; i++) {
+    
+		if (!isNaN(trArr[i])) {
+			return trArr;
+		}
+
+		if (trArr[i] === '--discard-next') {
+			trArr.splice(trArr[i - 1], 2);
+		} else if (trArr[i] === '--discard-prev') {
+			trArr.splice(trArr[i - 2], 2);
+		} else if (trArr[i] === '--double-prev') {
+			trArr[i - 1];
+			trArr[i] = trArr[i - 1];
+		} else if (trArr[i] === '--double-next') {
+			trArr[i + 1];
+			trArr[i] = trArr[i + 1];
+		}
+	}
+	return trArr;
 }
+// console.log(transform([1, 2, 3, '--discard-prev', 4, 5]), 'discard-prev');
+// console.log(transform([1, 2, 3, '--discard-next', 4, 5]), 'discard-next');
+// console.log(transform([1, 2, 3, '--double-next', 4, 5]), 'double-next'); //123445
+// console.log(transform([1, 2, 3, '--double-prev', 4, 5]), 'double-prev'); //123345
 
 module.exports = {
   transform
-};
+}
--- src/what-season.js
+++ src/what-season.js
@@ -12,48 +12,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function getSeason(date) {
-	// let month = Math.floor(((date.getMonth() / 12) * 4) % 4);
-	let since1970 =-Date.parse(date);
-  console.log(since1970, "1970");
-	if (date !== null || typeof date !=='undefined' || !isNaN(date) || date instanceof Date) {
-		//   return  'Invalid date!';
-		// }
 
-		let month = +date.getMonth() + 1;
-
-		switch (month) {
-			case 12:
-			case 1:
-			case 2:
-				season = 'winter';
-				break;
-			case 3:
-			case 4:
-			case 5:
-				season = 'spring';
-				break;
-			case 6:
-			case 7:
-			case 8:
-				season = 'summer';
-				break;
-			case 9:
-			case 10:
-			case 11:
-				season = 'fall';
-				break;
-			default:
-				throw new Error('Invalid date!');
-		}
+	if (typeof date === 'undefined') {
+		return 'Unable to determine the time of year!';
 	}
+
+	let month;
+
+	if (typeof date === 'number' && date > 0 && date <= 12) {
+	} else if (date instanceof Date === false || Object.getOwnPropertyNames(date).length) {
+		throw new Error('Invalid date!');
+	}
+	if (typeof date === 'number') {
+		month = date;
+	} else if (!isNaN(date)) {
+		month = +date.getMonth() + 1;
+	} else {
+  }
+
+	let season;
+
+	switch (month) {
+		case 12:
+		case 1:
+		case 2:
+			season = 'winter';
+			break;
+		case 3:
+		case 4:
+		case 5:
+			season = 'spring';
+			break;
+		case 6:
+		case 7:
+		case 8:
+			season = 'summer';
+			break;
+		case 9:
+		case 10:
+		case 11:
+			season = 'fall';
+			break;
+	}
+
+	// console.log('=================', season, 'season');
 	return season;
 }
 
-// module.exports = {
-// 	getSeason,
-// };
-console.log();
-getSeason(new Date(866, 2, 10, 12, 53, 10, 825));
+module.exports = {
+	getSeason,
+};
+//getSeason(new Date(866, 2, 10, 12, 53, 10, 825));
 // () => getSeason({ John: 'Smith' }),
 // () => getSeason(20192701),
 // () => getSeason([2019, '27', 0 + '1']),
